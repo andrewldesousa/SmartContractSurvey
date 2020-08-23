3280ec7e2548eb4310481796923027ef3d97e4e5
andrewldesousa <axd866@miami.edu>
2020-08-23 05:21:05 +0200

    Added theme provider and wrote a basic landing page

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,65 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+
+
+const useStyles = makeStyles((theme) => ({
+  landingPageButton: {
+    width: 400,
+    height: 60,
+    backgroundColor: theme.primary,
+    color: 'white',
+  },
+}));
+
 
 export default function Home() {
+  const classes = useStyles(useTheme());
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Smart Contract Survey</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Smart Contract Survey!</a>
         </h1>
-
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This survey is about blah blah blah. Although, it will take some time to complete. You can win X.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Button variant="contained" color="primary" className={classes.landingPageButton}>
+            Start the survey
+          </Button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
