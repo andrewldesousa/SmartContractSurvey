f69ca2e85ed6eabb4efe12caf6b3dfe80b4a5be7
navid <navid72m@gmail.com>
2020-08-25 01:40:21 +0200

    add pagination

--- app/pages/index.js
+++ app/pages/index.js
@@ -6,19 +6,49 @@ import DateQA from '../components/q_date'
 import DiscreteSlider from '../components/q_slider'
 import DropdownQA from '../components/q_dropdown'
 import Wallet from '../components/q_wallet'
+import Pagination from '@material-ui/lab/Pagination';
+
+
 
 export default function Home() {
   const qList=['A','B','C']
+  var elements = [<Wallet/>
+    ,<DropdownQA question='Choose one of the following' label='Some label' list={qList} />
+    ,<RateQA question = 'How good was ...?' />
+    ,<TextQA question ='What where when who why?' hint='Answer here' />
+    ,<SingleQA question ='Which of the following ...?' qList={qList}/>
+    ,<MultipleQA question ='Choose the relevent options.' qList={qList} />
+    ,<DateQA question ='When did .....?' label='Date of something' />
+    ,<DiscreteSlider question = 'What is the value of...?' label='Value of' min={0} max={100} step={10}/>]
+    const [currentPage, setcurrentPage] = React.useState(1);
+    
+    const [pageSize, setpageSize] = React.useState(5);
+    const [page, setPage] = React.useState(1);
+    const indexOfLastPost =  page * pageSize;
+    const indexOfFirstPost = indexOfLastPost - pageSize;
+    var paginationStyle = {
+      "textAlign": 'center',
+      padding: '1.5rem',
+      margin: '0 auto',
+      display: 'flex',
+      color: 'inherit',
+      display: 'block',
+      width: '70%',
+      transitionDuration: 'color 0.15s ease',
+      minHeight: '180px'
+    }
+    const handleChange = (event, value) => {
+
+      setPage(value);
+    };
+
+   
   return (
-    <div>
-      <Wallet/>
-      <DropdownQA question='Choose one of the following' label='Some label' list={qList} />
-      <RateQA question = 'How good was ...?' />
-      <TextQA question ='What where when who why?' hint='Answer here' />
-      <SingleQA question ='Which of the following ...?' qList={qList}/>
-      <MultipleQA question ='Choose the relevent options.' qList={qList} />
-      <DateQA question ='When did .....?' label='Date of something' />
-      <DiscreteSlider question = 'What is the value of...?' label='Value of' min={0} max={100} step={10}/>
+    <div id="Cards"   >
+     
+      {elements.slice(indexOfFirstPost, indexOfLastPost)}
+     
+      <Pagination count={2}  page={page} shape="rounded" style = {paginationStyle} onChange={handleChange} />
       <br/>
     </div>
   )
