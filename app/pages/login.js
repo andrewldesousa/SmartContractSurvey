9bd1b9e6ab3ba21d1099e5fa343f2dab85118f09
Madhavendrasingh <msnegi1997@gmail.com>
2020-09-01 19:36:36 +0200

    chekcin semi page work

--- app/pages/login.js
+++ app/pages/login.js
@@ -1,4 +1,59 @@
+import {Link, useHistory} from 'react-router-dom';
+import {authenticate, signin} from '../auth';
 import React from 'react';
+
+
+export default function Signin(props) {
+    const history = useHistory();
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    });
+
+    const {email, password, success, error} = values;
+
+    const handleChange = (event, name) => {
+        setValues({...values, error: false, [name]: event.target.value});
+    };
+
+    const clickSubmit = event => {
+        event.preventDefault();
+        setValues({...values, error: false});
+        signin({email, password}).then(data => {
+            if (data.error) {
+                setValues({...values, error: data.error, success: false});
+            } else {
+                authenticate(data, () => {
+                    history.push("/survey")
+                });
+            }
+        });
+    };
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    );
+
+    const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+    setTimeout(function () {
+        setCardAnimation("");
+    }, 700);
+    const classes = useStyles();
+    const {...rest} = props;
+
+    return(
+        <div></div>
+    );
+
+}
+
+
+
+/*import React from 'react';
 import { Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox, Card, CardHeader } from '@material-ui/core';
 import { Email, Https } from '@material-ui/icons'
 const styles = theme => ({
@@ -65,3 +120,4 @@ class LoginTab extends React.Component {
 }
 
 export default withStyles(styles)(LoginTab);
+*/
