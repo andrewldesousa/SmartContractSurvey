29a5cd9d8dc945b65627c7968d1fe71fdd67e9a4
andrewldesousa <axd866@miami.edu>
2020-09-09 00:53:43 +0200

    Fixed eslint errors

--- app/pages/survey.js
+++ app/pages/survey.js
@@ -31,28 +31,32 @@ export default function Home() {
               data.sections.map((section) => section.questions.map((question, i) => {
                 switch (question.type) {
                   case 'slider':
-                    setElement(elements => [...elements,<DiscreteSlider  key= {i} question = {question.prompt} label='' min={0} max={100} step={1}/>]);
+                    setElement((elements) => [...elements, <DiscreteSlider key={i} question={question.prompt}
+                      label='' min={0} max={100} step={1}/>]);
                     break;
                   case 'selectOne':
-                    setElement(elements => [...elements,<SingleQA key = {i} question ={question.prompt} qList={question.choices} />]);
+                    setElement((elements) => [...elements, <SingleQA key = {i}
+                      question ={question.prompt} qList={question.choices} />]);
                     break;
                   case 'selectOneOrOther':
-                    setElement(elements => [...elements,<MultipleQA key = {i} question ={question.prompt} qList={question.choices} />]);
+                    setElement((elements) => [...elements, <MultipleQA key = {i}
+                      question={question.prompt} qList={question.choices} />]);
                     break;
                   case 'number':
-                    setElement(elements => [...elements,<TextQA key = {i} question ={question.prompt} hint='Answer here' />]);
+                    setElement((elements) => [...elements, <TextQA key = {i} question ={question.prompt}
+                      hint='Answer here' />]);
                     break;
                   case 'rate':
-                    setElement(elements => [...elements,<RateQA key = {i} question ={question.prompt}/>]);
+                    setElement((elements) => [...elements, <RateQA key = {i}
+                      question ={question.prompt}/>]);
                     break;
                   case 'likert':
-                    setElement(elements => [...elements,<Likert key = {i} question = {question.prompt}/>]);
+                    setElement((elements) => [...elements, <Likert key = {i}
+                      question = {question.prompt}/>]);
                 }
               }
-              )
-              );
-          
-          //setItems(result);
+            )
+          )
         },
         
         (error) => {
@@ -62,45 +66,41 @@ export default function Home() {
       )
   }, [])
   var contents;
-  
-  
-    const [currentPage, setcurrentPage] = React.useState(1);
-    
-    const [pageSize, setpageSize] = React.useState(5);
-    const [page, setPage] = React.useState(1);
-    const indexOfLastPost =  page * pageSize;
-    const indexOfFirstPost = indexOfLastPost - pageSize;
-    const noOfpages = Math.ceil(elements.length/pageSize);
-    var paginationStyle = {
-      "textAlign": 'center',
-      padding: '1.5rem',
-      margin: '0 auto',
-      display: 'flex',
-      color: 'inherit',
-      display: 'block',
-      width: '70%',
-      transitionDuration: 'color 0.15s ease',
-      minHeight: '180px'
-    }
-    const handleChange = (event, value) => {
-
-      setPage(value);
-    };
+  const [currentPage, setcurrentPage] = React.useState(1);
+  const [pageSize, setpageSize] = React.useState(5);
+  const [page, setPage] = React.useState(1);
+  const indexOfLastPost = page * pageSize;
+  const indexOfFirstPost = indexOfLastPost - pageSize;
+  const noOfpages = Math.ceil(elements.length/pageSize);
+  const paginationStyle = {
+    textAlign: 'center',
+    padding: '1.5rem',
+    margin: '0 auto',
+    display: 'flex',
+    color: 'inherit',
+    display: 'block',
+    width: '70%',
+    transitionDuration: 'color 0.15s ease',
+    minHeight: '180px',
+  };
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   if (!isLoaded) {
     return <div>Loading...</div>;
-  } else { 
-  return (
+  } else {
+    return (
       <React.Fragment>
         <ButtonAppBar/>
         <br/>
         <br/>
-    <div id="Cards">  
-      {elements.slice(indexOfFirstPost,indexOfLastPost)}
-      <Pagination count={noOfpages}  page={page} shape="rounded" style = {paginationStyle} onChange={handleChange} />
-      <br/>
-    </div>
+        <div id="Cards">
+          {elements.slice(indexOfFirstPost, indexOfLastPost)}
+          <Pagination count={noOfpages} page={page} shape="rounded" style = {paginationStyle} onChange={handleChange} />
+          <br/>
+        </div>
       </React.Fragment>
-  );
-}
+    );
+  }
 }
