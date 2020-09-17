4c301478ef8c211e19fc2b94d8d7e824e92a0bf0
Madhavendrasingh <msnegi1997@gmail.com>
2020-09-17 03:10:04 +0200

    Api added for submisison
    survey page error to be resolved

--- app/pages/survey.js
+++ app/pages/survey.js
@@ -1,20 +1,20 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Pagination from '@material-ui/lab/Pagination';
 import useSWR from 'swr';
-
 import TextQA from '../components/questions/text';
 import RateQA from '../components/questions/rate';
 import SingleQA from '../components/questions/singleChoice';
 import MultipleQA from '../components/questions/multipleChoice';
 import DateQA from '../components/questions/date';
-import DiscreteSlider from '../components/questions/slider';
+import DiscreteSlider from '../components/questions/sliderDiscrete';
 import DropdownQA from '../components/questions/dropdown';
 import Wallet from '../components/questions/wallet';
 import Likert from '../components/questions/likert';
 import Button from '@material-ui/core/Button';
 import Spinner from '../components/spinner';
 import ButtonAppBar from '../components/header';
-
+import { useRouter } from 'next/router'
+import survey from './api/survey';
 
 const paginationStyle = {
   textAlign: 'center',
@@ -43,74 +43,95 @@ const spinnerStyle = {
   transform: 'translate(-50%, -50%)',
 };
 
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-function useSurvey() {
-  const {data, error} = useSWR('/api/survey', fetcher);
-  return {
-    survey: data,
-    isLoading: !error && !data,
-    isError: error,
-  };
-}
-
-function parseSurvey(survey) {
-  const questions = [<Wallet key={-1}/>];
-
-  survey.sections.map((section) => section.questions.map((question, i) => {
-    switch (question.type) {
-      case 'slider':
-        questions.push(<DiscreteSlider key={i} question={question.prompt} label='' min={0} max={100} step={1}/>);
-        break;
-      case 'selectOne':
-        questions.push(<SingleQA key={i} question ={question.prompt} qList={question.choices}/>);
-        break;
-      case 'selectOneOrOther':
-        questions.push(<MultipleQA key={i} question={question.prompt} qList={question.choices}/>);
-        break;
-      case 'number':
-        questions.push(<TextQA key = {i} question ={question.prompt} hint='Answer here'/>);
-        break;
-      case 'rate':
-        questions.push(<RateQA key={i} question ={question.prompt}/>);
-        break;
-      case 'likert':
-        questions.push(<Likert key={i} question={question.prompt}/>);
-    }
-  }));
-  questions.push(<Button variant="contained" style={submitStyle} color="primary" href="/submission">Submit</Button>);
-  return questions;
-}
-
-
-export default function Survey() {
-  const {survey, isLoading, isError} = useSurvey();
+ const Survey=()=> {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const router = useRouter();
+  const { survey, isLoading, isError } = useSurvey();
   const [pageSize, setPageSize] = React.useState(5);
   const [page, setPage] = React.useState(1);
+  const [redirect, setRedirect] = React.useState(false);
 
   const changePage = (event, value) => {
     setPage(value);
   };
 
-  if (isLoading) return <div style={spinnerStyle}><Spinner/></div>;
+  function useSurvey() {
+    const { data, error } = useSWR('/api/survey', fetcher);
+    return {
+      survey: data,
+      isLoading: !error && !data,
+      isError: error,
+    };
+  }
+  const fetcher1 = (...args) => fetch(...args).then((res) => res.json());
+
+  function submitBtn() {
+    const {data, error} = useSWR('/api/submit', fetcher1);
+    if (!error) {
+      setRedirect(true);
+    } else {
+      setRedirect(false);
+    }
+  }
+
+  const parseSurvey=(survey)=> {
+    const questions = [<Wallet key={-1} />];
+
+    survey.sections.map((section) => section.questions.map((question, i) => {
+      switch (question.type) {
+        case 'slider':
+          questions.push(<DiscreteSlider key={i} question={question.prompt} label='' min={0} max={100} step={1} />);
+          break;
+        case 'selectOne':
+          questions.push(<SingleQA key={i} question={question.prompt} qList={question.choices} />);
+          break;
+        case 'selectOneOrOther':
+          questions.push(<MultipleQA key={i} question={question.prompt} qList={question.choices} />);
+          break;
+        case 'number':
+          questions.push(<TextQA key={i} question={question.prompt} hint='Answer here' />);
+          break;
+        case 'rate':
+          questions.push(<RateQA key={i} question={question.prompt} />);
+          break;
+        case 'likert':
+          questions.push(<Likert key={i} question={question.prompt} />);
+      }
+    }));
+    questions.push(<Button variant="contained" style={submitStyle} color="primary" onClick={submitBtn} >Submit</Button>);
+    return questions;
+  }
+
+  if (isLoading) return <div style={spinnerStyle}><Spinner /></div>;
   if (isError) return <p>Error!</p>;
 
   const questions = parseSurvey(survey);
   const indexOfLastPost = page * pageSize;
   const indexOfFirstPost = indexOfLastPost - pageSize;
   const numOfpages = Math.ceil(questions.length / pageSize);
+  const questionList = () => (
+    <div id="Cards">
+      {questions.slice(indexOfFirstPost, indexOfLastPost)}
+      <Pagination count={numOfpages} page={page} shape="rounded" style={paginationStyle} onChange={changePage} />
+      <br />
+    </div>
+  )
+
+  const redirectUser = () => {
+    if (redirect) {
+      router.push('/submission')
+    }
+  };
 
   return (
-    <>
-      <ButtonAppBar/>
-      <br/>
-      <br/>
-      <div id="Cards">
-        {questions.slice(indexOfFirstPost, indexOfLastPost)}
-        <Pagination count={numOfpages} page={page} shape="rounded" style={paginationStyle} onChange={changePage} />
-        <br/>
-      </div>
-    </>
+    <div>
+      <ButtonAppBar />
+      <br />
+      <br />
+      {questionList()}
+      {redirectUser()}
+    </div>
   );
 }
+
+export default Survey;
