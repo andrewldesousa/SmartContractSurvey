3ba6067b91422937ffeff07a98ba3bc4b54eabfc
Madhavendrasingh <msnegi1997@gmail.com>
2020-10-07 00:41:12 +0200

    Extra survay get features added : Backend Version-1.0
    The backend for storage and retrival of data.

--- backend/controller/survey.js
+++ backend/controller/survey.js
@@ -37,7 +37,29 @@ exports.createSurvey = (req, res) => {
     });
 }
 
+exports.getSurveyByOwner = (req, res, next) => {
+    Survey.find({ 'owner': req.body.Oid }, function (err, data) {
+        if (err) console.log(err)
+        if (!data) {
+            return res.status(400).json({ err });
+        }
+        res.json(data)
+        next()
+    })
+}
+
 exports.getSurvey = (req, res, next) => {
+    Survey.find({ '_id': req.body.S_id }, function (err, data) {
+        if (err) console.log(err)
+        if (!data) {
+            return res.status(400).json({ err });
+        }
+        res.json(data)
+        next()
+    })
+}
+
+exports.getSurveyQuesitons = (req, res, next) => {
     Question.find({ 'survey_id': req.body.S_id }, function (err, data) {
         if (err) console.log(err)
         if (!data) {
--- backend/routes/survey.js
+++ backend/routes/survey.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {storeResult, createSurvey, storeQuestions, getResponse, getSurvey, getResponceCount} = require('../controller/survey');
-const { authenticate, authorize } = require('../controller/user');
+const {storeResult, createSurvey, storeQuestions, getResponse, getSurvey, getSurveyByOwner, getSurveyQuesitons, getResponceCount} = require('../controller/survey');
+const { authenticate, authorize, requireSignin } = require('../controller/user');
 
 router.post('/storeResult',storeResult);
-router.post('/createSurvey',authenticate, createSurvey);
+router.post('/createSurvey', requireSignin, authenticate, createSurvey);
 router.post('/storeQuestions',storeQuestions)
-router.get('/getResponse',getResponse)
-router.get('/getSurvey',getSurvey)
-router.get('/getResponceCount',getResponceCount)
+router.get('/getResponse', requireSignin, getResponse)
+router.get('/getSurveyQuesitons', getSurveyQuesitons)
+router.get('/getResponceCount', requireSignin, getResponceCount)
+router.get('/getSurvey', getSurvey)
+router.get('/getSurveyByOwner', requireSignin, getSurveyByOwner)
+
 
 module.exports = router;
