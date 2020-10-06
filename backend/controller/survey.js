b5d9349b654f1a02154f8645610cfd9552dbadb3
Madhavendrasingh <msnegi1997@gmail.com>
2020-10-06 23:05:32 +0200

    Counting responses added the option list not tested yet

--- backend/controller/survey.js
+++ backend/controller/survey.js
@@ -59,13 +59,60 @@ exports.getResponse = (req, res, next) => {
     })
 }
 
-exports.getResponceCount = (req, res, next) => {
-    Question.find({ 'survey_id': req.body.S_id }, function (err, data) {
+
+
+
+exports.getResponceCount = (req, res) => {
+    Question.find({ 'survey_id': req.body.S_id }, { '_id': 1 }, function (err, data) {
         if (err) console.log(err)
         if (!data) {
             return res.status(400).json({ err });
         }
-        /*var collect={}
+    }).then(function (data) {
+        var dataList=[]
+        for (i in data){
+            dataList.push(data[i]['_id'])
+        }
+        console.log(dataList)
+        Response.aggregate(
+            [
+                { $match: { 'question_id': {$in:dataList} } },
+                { $group: { _id: {Qid : '$question_id', answer:'$answer'}, count: { $sum: 1 } } }
+            ],
+            function (err1, data1) {
+                
+                if (err1) console.log(err1)
+                if (!data1) {
+                    return res.status(400).json({ err1 });
+                }
+                else{
+                    console.log(data1)
+                    res.json(data1)
+                }
+            }
+        )
+    })
+}
+
+
+exports.getResponceCountOld = (req, res) => {
+
+    Question.find({ 'survey_id': req.body.S_id }, { '_id': 1 }, function (err, data) {
+        if (err) console.log(err)
+        if (!data) {
+            return res.status(400).json({ err });
+        }
+    }).then(function (data) {
+        var collect = {}
+        for (var iter = 0; iter < data.length; ++iter) {
+            var temp = data[iter]['_id']
+            console.log(getX(temp))
+            collect = getX(temp)
+        }
+        //console.log(collect)
+        res.json(collect);
+    })
+    /*var collect={}
         var a = []
         var b = []
         for (i in data){
@@ -80,35 +127,43 @@ exports.getResponceCount = (req, res, next) => {
         for (c=0;c<a.length;++c){
             collect[a[c]] = b[c]
         }*/
-        var collect={}
-        for (var iter = 0; iter < data.length; ++iter) {
-            var temp = data[iter]['_id']
-            Response.find({ 'question_id': temp }, function (err1, Resdata) {
-                if (err1) console.log(err1)
-                if (!Resdata) {
-                    return res.status(400).json({ err1 });
-                }
-                var a = []
-                var b = []
-                for (var j = 0; j < Resdata.length; ++j) {
-                    if (!a.includes(Resdata[j]['answer'])) {
-                        a.push(Resdata[j]['answer'])
-                        b.push(1)
-                    }
-                    else {
-                        b[a.indexOf(Resdata[j]['answer'])]++;
-                    }
-                }
-                for (var j = 0; j < a.length; ++j) {
-                    collect[`${temp}`] = { "answer": a[j], "Count": b[j] }
-                    /*
-                    * The collect is not collecting but it is storing only the 
-                    * temp values for one option for a question  
-                    */
-                }
-            })
-        }
-        res.json(collect);
-        next();
+}
+
+function getX(temp) {
+    Response.find({ 'question_id': temp }, function (err1, Resdata) {
+        if (err1)
+            console.log(err1)
+    }).then(function (Resdata) {
+        return findIt(Resdata, temp)
     })
+}
+
+function findIt(Resdata, temp) {
+    var a = []
+    var b = []
+    var bucket = []
+    for (var j = 0; j < Resdata.length; ++j) {
+        if (!a.includes(Resdata[j]['answer'])) {
+            a.push(Resdata[j]['answer'])
+            b.push(1)
+        }
+        else {
+            b[a.indexOf(Resdata[j]['answer'])]++;
+        }
+    }
+    //console.log(combineIt(a,b,temp))
+    return combineIt(a, b, temp)
+}
+
+function combineIt(a, b, temp) {
+    var collect = {}
+    var bucket = []
+    for (var j = 0; j < a.length; ++j) {
+        collect[`${temp}`] = { "answer": a[j], "Count": b[j] }
+        console.log(collect)
+        if (bucket.indexOf(collect) == -1)
+            bucket.push(collect)
+    }
+    //console.log(bucket)
+    return bucket
 }
